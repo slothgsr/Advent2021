e973da8e9cf35d22f0b234fd3fa66810c441f17f
slothgsr <dustin.rego@gmail.com>
2021-12-11 14:39:03 -0500

    Day2-1 finished

---
+++ Day2/Day2-1.py
@@ -0,0 +1,22 @@
+file = (r'C:\Users\SlothGSR\Desktop\python_stuff\MyPythonScripts\Advent2021\Day2\input.txt')
+
+with open(file) as f:
+    lines = [line.rstrip('\n') for line in f]
+
+
+
+
+print(lines)
+
+forward = 0
+depth = 0
+
+for i in lines:
+    if "forward" in i:
+        forward += int(i[8])
+    if "down" in i:
+        depth += int(i[5])
+    if "up" in i:
+        depth -= int(i[3])
+
+print(forward * depth)
