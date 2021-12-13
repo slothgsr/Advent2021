b72576b868f7bd9cfabba1230de6e194ae9ed121
slothgsr <dustin.rego@gmail.com>
2021-12-13 12:36:19 -0500

    Day2 finished

--- Day1/Day1-1.py
+++ Day1/Day1-1.py
@@ -1,4 +1,9 @@
-file = (r'C:\Users\SlothGSR\Desktop\python_stuff\MyPythonScripts\Advent2021\Day1\input.txt')
+import os
+import sys
+
+abspath = os.path.abspath(sys.argv[0])
+dname = os.path.dirname(abspath)
+file = (r'day1\input.txt')
 
 with open(file) as f:
     lines = [int(line.rstrip('\n')) for line in f]
--- Day2/Day2-2.py
+++ Day2/Day2-2.py
@@ -3,6 +3,7 @@ import sys
 
 abspath = os.path.abspath(sys.argv[0])
 dname = os.path.dirname(abspath)
+
 file = (r'day2\input.txt')
 
 with open(file) as f:
@@ -10,13 +11,15 @@ with open(file) as f:
 
 forward = 0
 depth = 0
+aim = 0
 
 for i in lines:
     if "forward" in i:
         forward += int(i[8])
+        depth += aim * int(i[8])
     if "down" in i:
-        depth += int(i[5])
+        aim += int(i[5])
     if "up" in i:
-        depth -= int(i[3])
+        aim -= int(i[3])
 
 print(forward * depth)
