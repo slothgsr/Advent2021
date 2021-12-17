e032fd26a6871f580d7a592b2d2c4ef3e98c8ef5
slothgsr <dustin.rego@gmail.com>
2021-12-17 09:55:37 -0500

    D4 wip still

--- Day4/Day4-1.py
+++ Day4/Day4-1.py
@@ -3,36 +3,35 @@ import sys
 
 abspath = os.path.abspath(sys.argv[0])
 dname = os.path.dirname(abspath)
-
 file = (r'Day4\sample.txt')
 
 def makeboards(lines):
-    count = 0
     boardnum = 0
     boards = []
-    for i in lines[2:]:
-        print(list(i.split()))
+    count = 0
+    for i in lines[1:]:
         if i == '':
-             boardnum += 1
-        boards[boardnum].add(list(i.split()))
+            boards.append([])
+            boardnum += 1
+            count = 0
+        else:
+            boards[boardnum-1].append(i.split())
         count += 1
-        print("board num " + str(boardnum))
     return boards
         
-
-        
-        
-
-
-
 
 with open(file) as f:
     lines = [line.rstrip('\n') for line in f]
 called_numbers = lines[0]
 print(lines)
 print(called_numbers)
-
 print(makeboards(lines))
+
+# cboards = (makeboards(lines))
+# for i in cboards:
+#     for x in i:
+#         print(x)
+#     print('')
 
 
 
